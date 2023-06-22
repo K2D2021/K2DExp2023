a18a59bc87e328a0ecc5bb90bb71de8ad0d21ec6
K2D2021 <zxtreme@yandex.ru>
2023-06-22 23:43:13 +0300

    Add fun ResizeableText

--- app/src/main/java/com/example/k2dexp2023/MainActivity.kt
+++ app/src/main/java/com/example/k2dexp2023/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.k2dexp2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.k2dexp2023.ui.theme.K2DExp2023Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +24,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column() {
+                        ResizeableText("Hello world!", 14F)
+                        ResizeableText()
+                    }
                 }
             }
         }
     }
+}
+
+
+@Composable
+fun ResizeableText(displayedText: String = "Default text", textSize: Float = 10F) {
+    Text(text = displayedText, fontSize = textSize.sp, fontWeight = FontWeight.Thin)
 }
