72f5189b9afbaac271ef645af40c51bac735eed5
K2D2021 <zxtreme@yandex.ru>
2023-06-23 17:04:52 +0300

    Add interactive slider fun. Test with two sliders.

--- app/src/main/java/com/example/k2dexp2023/MainActivity.kt
+++ app/src/main/java/com/example/k2dexp2023/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.k2dexp2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.k2dexp2023.ui.theme.K2DExp2023Theme
 
@@ -24,9 +35,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column() {
-                        ResizeableText("Hello world!", 14F)
-                        ResizeableText()
+
+
+                    var sliderPosition by remember {
+                        mutableStateOf(30f)
+                    }
+                    var sliderPosition2 by remember {
+                        mutableStateOf(30f)
+                    }
+
+                    val handlePositionChange = { position: Float -> sliderPosition = position }
+                    val handlePositionChange2 = { position: Float -> sliderPosition2 = position }
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        ResizeableText("Welcome to Compose", sliderPosition)
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        SliderForTextSize(
+                            sliderPosition = sliderPosition,
+                            onPositionChange = handlePositionChange
+                        )
+
+                        Text(
+                            style = MaterialTheme.typography.headlineLarge,
+                            text = sliderPosition.toInt().toString() + "sp"
+                        )
+                        Spacer(modifier = Modifier.height(100.dp))
+
+                        ResizeableText("Second text", sliderPosition2)
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        SliderForTextSize(
+                            sliderPosition = sliderPosition2,
+                            onPositionChange = handlePositionChange2
+                        )
+                        Text(
+                            style = MaterialTheme.typography.headlineMedium,
+                            text = sliderPosition2.toInt().toString() + "sp"
+                        )
+
                     }
                 }
             }
@@ -37,5 +90,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ResizeableText(displayedText: String = "Default text", textSize: Float = 10F) {
-    Text(text = displayedText, fontSize = textSize.sp, fontWeight = FontWeight.Thin)
+    Text(
+        text = displayedText,
+        fontSize = textSize.sp,
+        fontWeight = FontWeight.Thin
+    )
+}
+
+@Composable
+fun SliderForTextSize(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(30.dp),
+        valueRange = 30f..38f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) })
+
 }
