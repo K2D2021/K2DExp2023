5576d621a2921a1bef098367d56fb1f56d257efe
K2D2021 <zxtreme@yandex.ru>
2023-06-28 23:08:08 +0300

    Add button initial logic and icons

--- app/src/main/java/com/example/k2dexp2023/MainActivity.kt
+++ app/src/main/java/com/example/k2dexp2023/MainActivity.kt
@@ -3,25 +3,29 @@ package com.example.k2dexp2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -129,8 +133,110 @@ fun CounterButton(
 
         DraggableThumbButton(
             value = value,
-            onClick = { /*TODO*/},
+            onClick = { /*TODO*/ },
             modifier = Modifier.align(Alignment.Center)
         )
     }
 }
+
+private const val ICON_BUTTON_ALPHA_INITIAL = 0.3f
+private const val CONTAINER_BACKGROUND_ALPHA_INITIAL = 0.3f
+
+@Composable
+fun ButtonContainer(
+    onValueDecreaseClick: () -> Unit,
+    onValueIncreaseClick: () -> Unit,
+    onValueClearClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    clearButtonVisible: Boolean = false
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(64.dp))
+            .background(Color.Black.copy(alpha = CONTAINER_BACKGROUND_ALPHA_INITIAL))
+            .padding(horizontal = 8.dp)
+    ) {
+        IconControlButton(
+            icon = ImageVector.vectorResource(id = R.drawable.baseline_expand_more_24),
+            contentDescription = "Decrease count",
+            onClick = onValueDecreaseClick,
+            tintColor = Color.White.copy(alpha = ICON_BUTTON_ALPHA_INITIAL)
+        )
+
+        if (clearButtonVisible) {
+            IconControlButton(
+                icon = ImageVector.vectorResource(id = R.drawable.baseline_clear_24),
+                contentDescription = "Clear count",
+                onClick = onValueClearClick,
+                tintColor = Color.White.copy(alpha = ICON_BUTTON_ALPHA_INITIAL)
+            )
+        }
+        IconControlButton(
+            icon = ImageVector.vectorResource(id = R.drawable.baseline_expand_less_24),
+            contentDescription = "Increase count",
+            onClick = onValueIncreaseClick,
+            tintColor = Color.White.copy(alpha = ICON_BUTTON_ALPHA_INITIAL)
+        )
+    }
+}
+
+@Composable
+private fun IconControlButton(
+    icon: ImageVector,
+    contentDescription: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    tintColor: Color = Color.White,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+            .size(48.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = contentDescription,
+            tint = tintColor,
+            modifier = Modifier.size(32.dp)
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
